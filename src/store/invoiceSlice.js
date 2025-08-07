a985e5f4ca3379e72938df29ca8a5944e2f5c86f
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-08-08 01:23:58 +0600

    Local Storage

--- src/store/invoiceSlice.js
+++ src/store/invoiceSlice.js
@@ -1,13 +1,43 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { addDays, format } from "date-fns";
 
-const initialState = {
-  invoices: [],
-  isFormOpen: false,
-  filter: "all",
-  selectedInvoice: null,
+//load local storage
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+
+    if (serializedState === null) {
+      return {
+        invoices: [],
+        isFormOpen: false,
+        filter: "all",
+        selectedInvoice: null,
+      };
+    }
+    return JSON.parse(serializedState);
+  } catch (error) {
+    return {
+      invoices: [],
+      isFormOpen: false,
+      filter: "all",
+      selectedInvoice: null,
+    };
+  }
 };
 
+const initialState = loadState();
+
+//save in local Storage
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//calculate whole amount
 const calculateAmount = (items) => {
   return items.reduce((sum, item) => {
     return sum + item.quantity * item.price;
@@ -28,6 +58,9 @@ const invoiceSlice = createSlice({
           format(addDays(new Date(), 30), "yyyy-MN-dd"),
       };
       state.invoices.push(newInvoice);
+      saveState(state);
+
+      state.isFormOpen = false;
     },
 
     toggleForm: (state) => {
